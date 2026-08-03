f9344c0a96508e6ef5ebcca49907c2bac4dd1ff1
James Koval <James.Ross.Koval@gmail.com>
2026-08-03 02:07:59 -0700

    cleanup whitespace

--- engine/header/VulkanPlugin.h
+++ engine/header/VulkanPlugin.h
@@ -551,8 +551,8 @@ private:
 
 	int last_key = -1;
 	glm::vec2 mouse_position ;
-	glm::vec2 mouse_wheel_position;
 	glm::vec2 mouse_down_position;
+	glm::vec2 mouse_wheel_position;
 	std::map<int, bool> mouse_down ;
 	bool mouse_hidden = false;
 	bool last_mouse_hidden = false; 

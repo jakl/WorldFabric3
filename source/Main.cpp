7a361c0abe6905ba876b1ecc6387bd8481788f3d
James Koval <James.Ross.Koval@gmail.com>
2026-09-25 03:08:19 -0700

    fix debug mode

--- source/Main.cpp
+++ source/Main.cpp
@@ -436,10 +436,7 @@ void setupGameStates() {
 	app->setState(Chess::ChessApp::state_name);
 }
 
-int debugMain(int argc, char* argv[]) {
-	CSVLog::findDesync({"server.csv", "client.csv"}, "time", 1.0) ;
-	
-}
+
 
 int exampleMain(int argc, char* argv[]) {
 	std::string command_line = argv[0];
@@ -569,4 +566,11 @@ int exampleMain(int argc, char* argv[]) {
 int main(int argc, char* argv[]) {
 	//Narball::main(argc, argv);
 	exampleMain(argc, argv);
+}
+
+int debugMain(int argc, char* argv[]) {
+	_set_error_mode(_OUT_TO_STDERR);
+	assert(false);
+	//CSVLog::findDesync({"server.csv", "client.csv"}, "time", 1.0) ;
+	return exampleMain(argc, argv);
 }
